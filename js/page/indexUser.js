7be6d0a79b3feb8cae6c90e07f1812030cde837d
ysss <yss_dlut@163.com>
2015-04-11 16:10:10 +0800

    more powerful

--- js/page/indexUser.js
+++ js/page/indexUser.js
@@ -13,3 +13,12 @@ $('#J_Logout').tap(function() {
 	plus.storage.clear();
 	plus.webview.getLaunchWebview().loadURL('../index.html');
 });
+
+$('#J_Other').on('tap', 'a', function(e) {
+	var $this = $(this);
+	e.preventDefault();
+	mui.openWindow('webview.html?title=' + encodeURIComponent($this.text()) + '&url=' + encodeURIComponent($this.attr('data-href')), 'webview.html', {
+		top: mui.HEIGHT_ZERO,
+		bottom: mui.HEIGHT_ZERO
+	});
+});
